a9c8040a1c8fb11ccfdc064eb00f943afdf3ce28
Alexfavero <alexfavero4036@gmail.com>
2025-02-24 16:04:48 -0300

    Exercicios 5 e 6

--- Lista1/exerc6.php
+++ Lista1/exerc6.php
@@ -1,14 +1,27 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio /title>
+    <title>Exercicio 6</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Exercicio </h1>
-    <form
+</head>
+
+<body>
+    <h1>Exercicio 6</h1>
+
+    <form method="post" action="exerc6resposta.php">
+
+        <div class="mb-3">
+            <label for="temperatura" class="form-label">Temperatura em ºC</label>
+            <input type="number" id="temperatura" name="temperatura" class="form-control" required="">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
