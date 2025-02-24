c9695b9ee9b32f72d26d7cc857ea2a0a494e8f54
Alexfavero <alexfavero4036@gmail.com>
2025-02-24 16:47:59 -0300

    exercicios 11 e 12

--- Lista1/exerc12resposta.php
+++ Lista1/exerc12resposta.php
@@ -1,13 +1,31 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio /title>
+    <title>Resposta12</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Exercicio </h1>
+</head>
+
+<body>
+    <h1>Resposta exercicio 12</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $base = $_POST['base'];
+            $expoente = $_POST['expoente'];
+            $resp = $base ** $expoente;
+
+            echo "$base elevado a $expoente = $resp";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
