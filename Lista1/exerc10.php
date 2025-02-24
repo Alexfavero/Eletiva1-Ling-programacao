f3fc6d33890cd9d7f9578911eba3e11f9ff907ee
Alexfavero <alexfavero4036@gmail.com>
2025-02-24 16:32:18 -0300

    exercicios 9 e 10

---
+++ Lista1/exerc10.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>exercicio 10</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exercicio 10</h1>
+    <form method="post" action="exerc10resposta.php">
+
+        <div class="mb-3">
+            <label for="largura" class="form-label">Informe a largura do retangulo</label>
+            <input type="number" id="largura" name="largura" class="form-control" required="">
+        </div>
+
+        <div class="mb-3">
+            <label for="altura" class="form-label">Informe a altura do retangulo</label>
+            <input type="number" id="altura" name="altura" class="form-control" required="">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
