7022857bcfdf610c4c9ad039a060cfcae39a15a4
Alexfavero <alexfavero4036@gmail.com>
2025-02-24 15:22:53 -0300

    exercicios 3 e 4 adicionados

---
+++ Lista1/exerc4resposta.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resposta4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Resposta exercicio 4</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $valor1 = $_POST['valor1'];
+            $valor2 = $_POST['valor2'];
+            if ($valor2 != 0) {
+                $sub = $valor1 / $valor2;
+                echo "O valor da multiplicação é: $sub";
+            } else {
+                echo "Não é possivel dividir o primeiro número por 0";
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
