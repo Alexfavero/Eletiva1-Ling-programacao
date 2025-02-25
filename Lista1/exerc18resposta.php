dbf7df3e2327b6d7aebd2046487bdf9f14b4f520
Alexfavero <alexfavero4036@gmail.com>
2025-02-25 09:33:49 -0300

    exercicios 17 e 18

---
+++ Lista1/exerc18resposta.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resposta18</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Resposta exercicio 18</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $capital = $_POST['capital'];
+            $taxa = $_POST['taxa'];
+            $periodo = $_POST['periodo'];
+            $juros = $capital * ((1 + ($taxa / 100)) ** $periodo);
+            echo "Juros composto: " . number_format($juros, 2) . " R$";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
