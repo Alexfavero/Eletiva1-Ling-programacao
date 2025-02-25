3119174b99b67637ca369a866143401eb5f2f291
Alexfavero <alexfavero4036@gmail.com>
2025-02-25 09:02:05 -0300

    exercicios 15 e 16

--- Lista1/exerc15resposta.php
+++ Lista1/exerc15resposta.php
@@ -1,13 +1,30 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio </title>
+    <title>Resposta15</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Exercicio </h1>
+</head>
+
+<body>
+    <h1>Resposta exercicio 15</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $peso = $_POST['peso'];
+            $altura = $_POST['altura'];
+            $imc = $peso / ($altura ** 2);
+            echo "Seu IMC é de $imc";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
