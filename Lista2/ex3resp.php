71d47e04d9d720c15afba76c7ca0497b0b555c41
Alexfavero <alexfavero4036@gmail.com>
2025-03-06 21:28:13 -0300

    alteração lista 2 ex 3
    exclusão de linha com variável não usada

--- Lista2/ex3resp.php
+++ Lista2/ex3resp.php
@@ -15,7 +15,6 @@
         try {
             $num1 = $_POST['num1'];
             $num2 = $_POST['num2'];
-            $dif = $num1 - $num2;
 
             if ($num1 == $num2) {
                 echo "$num1. Os números inserido são iguais";
